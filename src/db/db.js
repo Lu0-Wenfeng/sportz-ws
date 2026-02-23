99dffc5b32f71cda7f4ed4509cda68b04d7b486e
Luo Wenfeng <93787538+Lu0-Wenfeng@users.noreply.github.com>
2026-02-23 15:41:25 -0800

    Feat: Implement database schemas (#1)
    * implement database schemas
    
    * Update src/db/db.js
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ src/db/db.js
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined');
+}
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected PG client error", err);
+  process.exit(-1);
+});
+
+export const db = drizzle(pool);
